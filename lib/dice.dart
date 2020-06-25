b0edbd21dfa97b11a5347ba8390c4bf1e9a437c3
Hamdan985 <hamdan246@gmail.com>
2020-06-25 13:43:04 +0530

    Cheat added

--- lib/dice.dart
+++ lib/dice.dart
@@ -7,8 +7,12 @@ class Dice extends StatefulWidget {
 }
 
 class _DiceState extends State<Dice> {
+  bool isCheatActivated = false;
   int leftDiceNumber;
   int rightDiceNumber;
+
+  int cheatLeftNumber;
+  int cheatRightNumber;
 
   @override
   void initState() {
@@ -19,14 +23,81 @@ class _DiceState extends State<Dice> {
 
   void diceChange() {
     setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
+      if (!isCheatActivated) {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      }
+      else{
+        leftDiceNumber = cheatLeftNumber;
+        rightDiceNumber = cheatRightNumber;
+        isCheatActivated = false;
+      }
     });
   }
 
-  void cheat() {
-    print('inside cheat');
-    //TODO : 1 - add modalBottomSheet here with input for the cheat number
+  void handleChange() {
+    setState(() {
+      print(leftDiceNumber);
+      print(rightDiceNumber);
+      isCheatActivated = true;
+    });
+  }
+
+  void cheat(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              color: Colors.deepOrangeAccent,
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                            onChanged: (value) {
+                              cheatLeftNumber = int.parse(value);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                            onChanged: (value) {
+                              cheatRightNumber = int.parse(value);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    RaisedButton(
+                      color: Colors.greenAccent,
+                      onPressed: () {
+                        Navigator.pop(context);
+                        handleChange();
+                      },
+                      child: Text('Cheat'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
   }
 
   @override
@@ -47,7 +118,7 @@ class _DiceState extends State<Dice> {
                 child: FlatButton(
                   child: Image.asset('assets/images/dice$leftDiceNumber.png'),
                   onPressed: diceChange,
-                  onLongPress: cheat,
+                  onLongPress: () => cheat(context),
                   padding: EdgeInsets.all(0),
                 ),
               ),
@@ -55,7 +126,7 @@ class _DiceState extends State<Dice> {
                 child: FlatButton(
                   child: Image.asset('assets/images/dice$rightDiceNumber.png'),
                   onPressed: diceChange,
-                  onLongPress: cheat,
+                  onLongPress: () => cheat(context),
                   padding: EdgeInsets.all(0),
                 ),
               ),
